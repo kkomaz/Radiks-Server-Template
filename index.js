fb4c9beebff2de431f040e7b614965a22b4b0e12
Alex Lee <alex@uniteus.com>
2019-04-13 12:10:07 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const { setup } = require('radiks-server')
+
+const app = express()
+
+setup({
+  mongoDBUrl: 'mongodb://localhost:27017/my-custom-database'
+}).then((RadiksController) => {
+  app.use('/radiks', RadiksController)
+})
+
+const PORT = process.env.PORT || 5000;
+
+app.listen((PORT), () => {
+  console.log('listening on port ' + PORT);
+});
