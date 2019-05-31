bea8a48f3146d325505e433721d186230e6f4ecd
Alex Lee <alex@uniteus.com>
2019-05-31 11:09:39 -0400

    CORS clean-up

--- index.js
+++ index.js
@@ -3,6 +3,24 @@ const { setup } = require('radiks-server')
 
 const app = express()
 
+app.use((req, res, next) => {
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', '*');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
+
 setup({
   mongoDBUrl: 'mongodb://localhost:27017/your-list'
 }).then((RadiksController) => {
